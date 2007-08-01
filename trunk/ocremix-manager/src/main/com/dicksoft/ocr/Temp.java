7fda7c34d2731dfc0c7a27f1479f36aa2a32f3b9
ryo.away <ryo.away@55a5df56-7d36-0410-9af3-517bc739de10>
2007-08-01 03:58:36 +0000

    working on JAXB

--- trunk/ocremix-manager/src/main/com/dicksoft/ocr/Temp.java
+++ trunk/ocremix-manager/src/main/com/dicksoft/ocr/Temp.java
@@ -13,39 +13,70 @@
 package com.dicksoft.ocr;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-import com.dicksoft.ocr.net.HttpUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import sun.security.provider.MD5;
+
+import com.dicksoft.ocr.util.HttpUtil;
+import com.dicksoft.ocr.util.StringUtil;
 
 /**
  * @author <a href="mailto:ryo.away@gmail.com">Richard Taylor</a>
  */
 public class Temp {
-    private static final String TOTAL_GAME_PREFIX = "games&nbsp;1 to 100 of ";
-    private static final int TOTAL_GAME_MAX_DIGITS = 30;
-    private static final int NUM_GAMES_PER_PAGE = 100;
+    // private static final String TOTAL_GAME_PREFIX = "games&nbsp;1 to 100 of
+    // ";
+    // private static final int TOTAL_GAME_MAX_DIGITS = 30;
+    // private static final int NUM_GAMES_PER_PAGE = 100;
+    private static final Log LOG = LogFactory.getLog(Temp.class);
 
     /**
      * @param args
      */
     public static void main(String[] args) {
+        LOG.info("Fetching");
         String result = null;
-        try {
-            result =
-                    HttpUtil
-                            .fetchText("http://ocremix.org/games/?&offset=0&limit="
-                                    + NUM_GAMES_PER_PAGE + "&sort=");
-        } catch (IOException e) {
-            e.printStackTrace();
+        boolean done = false;
+        for (int i = 1; !done; i++) {
+            try {
+                result =
+                        HttpUtil.fetchText("http://ocremix.org/remix/OCR"
+                                + String.format("%1$05d", i) + "/?style=xml");
+                 System.out.println(result);
+                String game =
+                        StringUtil.getBetween(result,
+                                "<html>\n  <head>\n    <title>ReMix: ", "\n");
+                
+                if (game==null || game.equals(""))
+                    done = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            done = true;// TODO
         }
-        // //
-        int totalGames =
-                Integer.parseInt(result.substring(
-                        result.indexOf(TOTAL_GAME_PREFIX)
-                                + TOTAL_GAME_PREFIX.length(),
-                        TOTAL_GAME_MAX_DIGITS).split(" ")[0]);
-        int numPages = (int) Math.ceil(totalGames / NUM_GAMES_PER_PAGE);
-        for (int i = 2; i <= numPages; i++) {
 
-        }
+        // ////////////
+        // try {
+        // result =
+        // HttpUtil
+        // .fetchText("http://ocremix.org/games/?&offset=0&limit="
+        // + NUM_GAMES_PER_PAGE + "&sort=");
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+        // int totalGames =
+        // Integer.parseInt(result.substring(
+        // result.indexOf(TOTAL_GAME_PREFIX)
+        // + TOTAL_GAME_PREFIX.length(),
+        // TOTAL_GAME_MAX_DIGITS).split(" ")[0]);
+        // int numPages = (int) Math.ceil(totalGames / NUM_GAMES_PER_PAGE);
+        // for (int i = 2; i <= numPages; i++) {
+        //
+        // }
     }
 }
